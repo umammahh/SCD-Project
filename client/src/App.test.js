93cded8e7500dc73349d6ee7103df64cb2bcced5
Umama <Umamaqasim420@gmail.com>
2022-11-15 04:19:46 +0500

    done

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import Home from './pages/Home';
+import Details from './pages/Details';
 import App from './App';
 test('renders Update Customer and related components', () => {
     render(<Home />)
     const linkElement = screen.getByTestId("add");
     expect(linkElement).toBeInTheDocument();
-    //expect(screen.getByRole("heading")).toHaveTextContent(/Admin Panel/);
     expect(screen.getByRole("button", {name: "Add user"})).toBeInTheDocument;
     const inputNode = screen.getByPlaceholderText('Email');
     expect(inputNode).toBeInTheDocument;
@@ -14,6 +15,8 @@ test('renders Update Customer and related components', () => {
     expect(inputNode3).toBeInTheDocument;
     const inputNode4 = screen.getByPlaceholderText('Age');
     expect(inputNode4).toBeInTheDocument;
+    const linkElement2 = screen.getByTestId("onDelete");
+    expect(linkElement2).toBeInTheDocument();
 });
 
 test('renders Update Customer and related components', () => {
